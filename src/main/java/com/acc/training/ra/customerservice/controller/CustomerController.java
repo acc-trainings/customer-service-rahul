554c90eb5645df6488c38377aa8178ca6a75f1b4
rahul0309 <rahul.a0309@gmail.com>
2021-01-07 00:00:06 -0500

    corrected build failure and updated POM

--- src/main/java/com/acc/training/ra/customerservice/controller/CustomerController.java
+++ src/main/java/com/acc/training/ra/customerservice/controller/CustomerController.java
@@ -5,10 +5,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CustomerController {
- 
+
     @GetMapping("/health")
-	public String health() {
-		return "Alive and kicking";
+    public String health() {
+        return "Alive and kicking";
     }
+
+    /*
+    @Override
+    public ResponseEntity<Customer> getCustomer(String ID) {
+        // TODO Auto-generated method stub
+        Customer cust = new Customer();
+        cust.setId("12345");
+        cust.setCustomerName("Rahul");
+        cust.setCustomerId("12345");
+        cust.setCustomerAddress("250 Main");
+        return ResponseEntity.status(HttpStatus.OK).body(cust);
+    }*/
+
+
+
+    
     
 }
